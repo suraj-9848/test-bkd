d668ac719fb112d66a6c6766f2db82c6e7a07a9d
bayyana-kiran <bayyanakiran@gmail.com>
2025-04-13 20:32:21 +0530

    fix: update entities path in MysqlConfig for correct model loading

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -4,10 +4,10 @@ import { config } from "./config";
 export const MysqlConfig: DataSourceOptions = {
   url: config.MYSQL_DATABASE_URL,
   type: "mysql",
-  synchronize: process.env.NODE_ENV !== "production", 
-  dropSchema: false, 
+  synchronize: process.env.NODE_ENV !== "production",
+  dropSchema: false,
   logging: process.env.NODE_ENV === "production" ? false : true,
-  entities: [__dirname + "/db/mysqlModels/*.js"],
+  entities: ["build/db/mysqlModels/*.js"],
   migrations: [],
   subscribers: [],
 };
